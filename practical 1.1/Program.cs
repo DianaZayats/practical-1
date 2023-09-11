c32318621f3d7d0e0dc45226369149736453d8a2
DianaZayats <marusya3651@gmail.com>
2023-09-11 18:18:29 +0300

    Добавьте файлы проекта.

---
+++ practical 1.1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Random random = new Random();
+
+        Console.WriteLine("Генерація псевдовипадкової послідовності чисел:");
+        for (int i = 0; i < 10; i++)
+        {
+            int randomNumber = random.Next(1, 101); 
+            Console.WriteLine(randomNumber);
+        }
+
+        int firstRandomNumber = random.Next(1, 101);
+        int secondRandomNumber = random.Next(1, 101);
+
+        Console.WriteLine($"Перше випадкове число: {firstRandomNumber}");
+        Console.WriteLine($"Друге випадкове число: {secondRandomNumber}");
+
+        if (firstRandomNumber == secondRandomNumber)
+        {
+            Console.WriteLine("Обидва числа однакові.");
+        }
+        else
+        {
+            Console.WriteLine("Числа різні.");
+        }
+    }
+}
---
+++ practical 1.2/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+
+class Program
+{
+    static void Main()
+    {
+        int sequenceLength = 5;
+
+        int[] randomNumbers = new int[sequenceLength];
+
+        using (RNGCryptoServiceProvider rng = new RNGCryptoServiceProvider())
+        {
+            byte[] randomBytes = new byte[4];
+
+            for (int i = 0; i < sequenceLength; i++)
+            {
+                rng.GetBytes(randomBytes);
+                int randomNumber = BitConverter.ToInt32(randomBytes, 0);
+                randomNumbers[i] = randomNumber;
+            }
+        }
+
+        Console.WriteLine("Послідовність криптографічно стійких випадкових цілих чисел:");
+        Console.WriteLine(string.Join(", ", randomNumbers));
+    }
+}
